c6f6778b569e47fea2857e8934d405d23ea61bd9
Eitan <278788152+eitan-dardik@users.noreply.github.com>
2026-09-10 10:30:32 +0300

    change SiteLayout.jsx to import logo from config.json

--- apps/web/src/components/SiteLayout.jsx
+++ apps/web/src/components/SiteLayout.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import { LOCATIONS } from './LocationsTable';
+import config from '../../../../landing-template/config.json';
 
-const LOGO_URL = 'https://images.hostinger.com/3d20ebf6-76ff-476e-8eb3-d0e2e4a9de2e.png';
 const FACEBOOK_URL = 'https://www.facebook.com/NorthlineDentalGroup';
 
 const NAV_ITEMS = [
@@ -18,7 +18,7 @@ export default function SiteLayout() {
         <div className="page-wrap">
             <header className="site-header">
                 <Link to="/" className="logo">
-                    <img src={LOGO_URL} alt="Northline Dental Group" width="240" height="90" />
+                    <img src={config.brand.logo} alt="Northline Dental Group" width="240" height="90" />
                 </Link>
                 <div className="header-right">
                     <p className="header-tag">Family &amp; Cosmetic Dentistry</p>
